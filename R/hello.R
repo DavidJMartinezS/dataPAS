e47d5a8abc3da03cfda988677b2f6e28aaf852d9
DavidJMartinezS <david.tinez1@gmail.com>
2024-05-31 13:07:02 -0400

    Initial commit

---
+++ R/hello.R
@@ -0,0 +1,15 @@
+install.packages("tibble")
+library(sf)
+
+comunas <- read_sf("n:/Dashboard PAS 150/COMUNAS/COMUNAS_v1.shp")
+
+write_sf(comunas, "./Data_SIG/comunas.gdb")
+read_sf("./Data_SIG/comunas.gdb")
+
+red_hidro <- read_sf("n:/Dashboard PAS 150/Hidrografia_V2/Hidrografia_V2 (1).shp")
+write_sf(red_hidro, "./Data_SIG/red_hidro.gdb")
+read_sf("./Data_SIG/red_hidro.gdb")
+
+red_vial <- read_sf("n:/Dashboard PAS 150/Red_Vial_Chile_18_07_2023.gdb")
+write_sf(red_vial, "./Data_SIG/red_vial.gdb")
+read_sf("./Data_SIG/red_vial.gdb")
