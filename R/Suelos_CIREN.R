7a25a7f7d77c9428c31ab9c4dccf09a6be79874d
DavidJMartinezS <david.tinez1@gmail.com>
2024-06-06 09:23:48 -0400

    add soil data

---
+++ R/Suelos_CIREN.R
@@ -0,0 +1,50 @@
+#' Suelos CIREN
+#'
+#' Datos geoespaciales de suelos en Chile desde Valparaíso a Los Lagos.
+#' Estos se presentan en coordenadas WGS84 (EPSG:4326).
+#'
+#' @name Suelos_CIREN
+#' @format
+#' \describe{
+#'   \item{DESCEROS}{Descripción de la erosión del suelo}
+#'   \item{DESCPEND}{Descripción 1 de la pendiente del suelo}
+#'   \item{DESCPROF}{Descripción 2 de la profundidad del suelo}
+#'   \item{DESCSUCA}{Descripción 1 de la sub-capacidad de uso del suelo}
+#'   \item{MSLINK}{Identificador único del elemento gráfico}
+#'   \item{NOMBSERI}{Nombre de la serie}
+#'   \item{PORCPOLI}{Porcentaje de suelo en la parametría indicada}
+#'   \item{SIMBSERI}{Símbolo de la serie de suelo}
+#'   \item{TEXTCAUS}{Descripción de la capacidad de uso del suelo}
+#'   \item{TEXTPEND}{Descripción 2 de la pendiente del suelo}
+#'   \item{TEXTPROF}{Descripción 2 de la profundidad del suelo}
+#'   \item{TEXTSUCA}{Descripción 2 de la sub-capacidad de uso del suelo}
+#' }
+#' @source <https://www.ciren.cl/>
+"suelo_ciren_r05"
+
+#' @rdname Suelos_CIREN
+"suelo_ciren_r06"
+
+#' @rdname Suelos_CIREN
+"suelo_ciren_r07"
+
+#' @rdname Suelos_CIREN
+"suelo_ciren_r08"
+
+#' @rdname Suelos_CIREN
+"suelo_ciren_r09"
+
+#' @rdname Suelos_CIREN
+"suelo_ciren_r10_cont"
+
+#' @rdname Suelos_CIREN
+"suelo_ciren_r10_chil"
+
+#' @rdname Suelos_CIREN
+"suelo_ciren_r13"
+
+#' @rdname Suelos_CIREN
+"suelo_ciren_r14"
+
+#' @rdname Suelos_CIREN
+"suelo_ciren_r16"
